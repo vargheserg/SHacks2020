bbe74530d651f17ada7f04ed997ac67c8c1652d3
Farhan Mohammed <farhanhm12@gmail.com>
2020-08-13 19:15:18 -0400

    f

--- scraper/definition.py
+++ scraper/definition.py
@@ -6,16 +6,16 @@ import json
 
 
 
-base_url = "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/investment-types/bonds.html"
-ido='button--bellow-6ea716ab-ade3-4607-93ce-29b5055e2804-body'
+base_url = "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/investment-types/options.html"
+term='Bonds'
 
-def parse_definition(url,idOFGlossary):
+def parse_definition(url,term):
     definitions = {}
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
     # print(soup)
     # the digit is the nth accordian on the page
-    digit = 1
+    digit = 2
     reference = soup.find_all( class_='button--bellow-body-content')[digit].find_all('div')[0].find_all('p')
     glossary =[[t.replace('\xa0', '') for t in i.text.splitlines()] for i in reference]
    
@@ -26,6 +26,9 @@ def parse_definition(url,idOFGlossary):
                 return False
         return True
     glossary=list(filter(filterWaste,glossary))
+    out = {}
+    for [term,defi] in glossary:
+        out[term]={"defi":defi, "mention":{"link":base_url,"term":term}}
     # reference = reference.find(attrs={"role" : "tabpanel"})
     # reference=reference.find('div').find('div')
 
@@ -34,8 +37,8 @@ def parse_definition(url,idOFGlossary):
     #     key = define.find('b').get_text().strip()
     #     definitions[key]= define.find('br').next_siblings
     # return definitions
-    return glossary
+    return out
 
 
 
-print(parse_definition(base_url,ido))
+print(parse_definition(base_url,term))
