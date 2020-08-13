050dfeba5459b0f21be01140dc7015afc54d67c1
Farhan Mohammed <farhanhm12@gmail.com>
2020-08-13 18:23:23 -0400

    Finished scraper

--- scraper/definition.py
+++ scraper/definition.py
@@ -7,17 +7,35 @@ import json
 
 
 base_url = "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/investment-types/bonds.html"
+ido='button--bellow-6ea716ab-ade3-4607-93ce-29b5055e2804-body'
 
-
-def parse_definition(url):
+def parse_definition(url,idOFGlossary):
     definitions = {}
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
-    
-    reference = soup.find('div', class_='section--content').find('div', class_='accordian-container').find('div', class_='button--bellow-cdfdbf0f-db21-4cbe-94fd-874e2173c1e7-body')
-    glossary = reference.find('div', class_='cmp cmp-text').find_all('p')
-    for def in glossary
-    key = def.find('b').get_text().strip()
-    definitions[key]= def.find('br').next_siblings
-    return definitions
-print(parse_definition(base_url))
+    # print(soup)
+    # the digit is the nth accordian on the page
+    digit = 1
+    reference = soup.find_all( class_='button--bellow-body-content')[digit].find_all('div')[0].find_all('p')
+    glossary =[[t.replace('\xa0', '') for t in i.text.splitlines()] for i in reference]
+   
+
+    def filterWaste(l):
+        for item in l:
+            if (len(item)<=1):
+                return False
+        return True
+    glossary=list(filter(filterWaste,glossary))
+    # reference = reference.find(attrs={"role" : "tabpanel"})
+    # reference=reference.find('div').find('div')
+
+    # glossary = reference.find_all('p')
+    # for define in glossary:
+    #     key = define.find('b').get_text().strip()
+    #     definitions[key]= define.find('br').next_siblings
+    # return definitions
+    return glossary
+
+
+
+print(parse_definition(base_url,ido))
